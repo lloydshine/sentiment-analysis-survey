74436d9eccdd097f6d27d4e174f3200704368f1f
lloydshine <semblantelloyd500@gmail.com>
2024-02-11 09:30:21 +0800

    ahhh

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,7 @@
 import { SurveyList } from "@/components/survey-list";
 import { Suspense } from "react";
 
-export default async function Home() {
+export default function Home() {
   return (
     <main>
       <Suspense fallback={<>Loading Surveys...</>}>
--- src/app/(main)/view/[surveyId]/loading.tsx
+++ src/app/(main)/view/[surveyId]/loading.tsx
@@ -1,3 +1,9 @@
+import { CircleBackslashIcon } from "@radix-ui/react-icons";
+
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <main className="flex justify-center items-center h-80">
+      <CircleBackslashIcon className="w-[40px] h-[40px] animate-spin" />
+    </main>
+  );
 }
